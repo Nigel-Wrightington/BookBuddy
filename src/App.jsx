72c0ce855851924d992f390b98999cf838082c4e
Chris Warman <actualchrisw@gmail.com>
2025-10-14 03:30:06 -0600

    Add files via upload
    app updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Link } from "react-router-dom";
+import BooksList from "./Pages/BookList";
+import BookDetails from "./Pages/BookDetails";
+import Account from "./Pages/Account";
 import Login from "./Pages/Login";
 import RegisterPage from "./Pages/RegisterPage";
-import Home from "./Pages/Books";
-import AccountPage from "./Pages/Account";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/account" element={<AccountPage />} />
-    </Routes>
+    <>
+      <nav className="nav">
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/account">Account</Link>
+          <Link to="/login">Log In</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<BooksList />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
   );
 }
